16b3e581f19f5d32854da1e7fa10fbe1c8be6b5d
Alex Nussbaumer <alex.nssbmr@gmail.com>
2022-05-07 18:23:25 +0200

    server is up

---
+++ server/index.ts
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import logger from 'morgan';
+import path from 'path';
+
+dotenv.config({ path: path.resolve(__dirname, `./config/${process.env.NODE_ENV}.env`) });
+
+const PORT = process.env.PORT;
+
+export const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(logger('dev'));
+
+export const server = app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT} 🚀`));
