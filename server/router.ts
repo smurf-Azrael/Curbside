6dd4422571d3894e954e2dd64b2089ebabb71eac
Alex Nussbaumer <alex.nssbmr@gmail.com>
2022-05-09 17:28:10 +0200

    feat: added auth middleware

--- server/router.ts
+++ server/router.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import usersController from './controllers/users.controller';
+import { authMiddleware } from './middlewares/auth.middleware';
 
 export const router = Router();
 
-router.post('/users', usersController.addInitialUser);
+router.post('/users', authMiddleware, usersController.addInitialUser);
 
-router.patch('/users/:id', usersController.finalizeUser);
+router.patch('/users/:id', authMiddleware, usersController.finalizeUser);
