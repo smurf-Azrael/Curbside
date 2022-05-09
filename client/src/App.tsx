0ebae8b9b241238bcd1a29d5a9722d2d47d4c747
smurf-Azrael <phillwang117@gmail.com>
2022-05-09 10:10:05 +0200

    feat(app.tsx): added routes
    Created all the routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,8 +4,6 @@ import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 
 function App() {
 
-
-
   return (
     <div className="App">
       <BrowserRouter>
@@ -17,7 +15,12 @@ function App() {
           <Route path="/signup" />
           <Route path="/verify" />
           <Route path="/set-profile" />
-
+          <Route path="/listing/:id" />
+          <Route path="/profile/:id" />
+          <Route path="/favorites" />
+          <Route path="chats" />
+          <Route path="/chats/:id" />
+          <Route path="/add-listing" />
         </Routes>
       </BrowserRouter>
     </div>
