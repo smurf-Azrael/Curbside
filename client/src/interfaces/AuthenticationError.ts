b1b9ab441395ccb02145525d63a28c0cc772d131
Mathieu Tinoco <tinoco.mathieu@gmail.com>
2022-05-10 10:37:30 +0200

    perf: implemented ts for signUp & LogIn and associated context

---
+++ client/src/interfaces/AuthenticationError.ts
@@ -0,0 +1,11 @@
+export interface SignUpError {
+  email?: string,
+  password?: string,
+  password2?: string,
+  globalError?: string,
+}
+export interface LogInError {
+  email?: string,
+  password?: string,
+  globalError?: string,
+}
