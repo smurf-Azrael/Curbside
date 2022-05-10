482692a46a36082f726fa184e4008038872b4ec6
Mathieu Tinoco <tinoco.mathieu@gmail.com>
2022-05-10 19:51:58 +0200

    feat(anthentication-finished): removed verify email from authentication process

--- client/src/views/SignupView.tsx
+++ client/src/views/SignupView.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import Signup from './../components/Signup';
 import { Container } from 'react-bootstrap';
 import "bootstrap/dist/css/bootstrap.min.css";
-import ButtonSmall from "../components/ButtonSmall";
-import Footer from "../components/Footer";
 import Header from '../components/Header';
 import '../styling/Authentication.css';
 
