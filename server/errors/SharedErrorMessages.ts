368f8b6b826e4e7bbf3744ca5c20b7d98267565c
smurf-Azrael <phillwang117@gmail.com>
2022-05-10 16:54:00 +0200

    feat: added get route for /users/:id

--- server/errors/SharedErrorMessages.ts
+++ server/errors/SharedErrorMessages.ts
@@ -4,3 +4,4 @@ export const USER_PARSING_ERROR = 'Unable to parse user data.';
 export const LISTING_PARSING_ERROR = 'Unable to parse listing.';
 
 export const USER_NOT_AUTHENTICATED = 'User not authenticated.';
+export const USER_NOT_FOUND = 'User not found.';
