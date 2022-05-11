2841081f87ba150d4034c4dbb75061f8bb3e5dfb
Alex Nussbaumer <alex.nssbmr@gmail.com>
2022-05-11 17:00:47 +0200

    fix: catch all route

--- server/router.ts
+++ server/router.ts
@@ -1,7 +1,8 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
 import usersController from './controllers/users.controller';
 import listingsController from './controllers/listings.controller';
 import { loginRequired } from './middlewares/login-required.middleware';
+import { PAGE_NOT_FOUND } from './errors/SharedErrorMessages';
 
 export const router = Router();
 
@@ -14,3 +15,7 @@ router.post('/listings', loginRequired, listingsController.addListing);
 router.get('/listings', listingsController.getListings);
 
 router.get('/users/:id', usersController.getUserProfile);
+
+router.get('*', (req: Request, res: Response) => {
+  res.status(404).send({ error: PAGE_NOT_FOUND });
+});
