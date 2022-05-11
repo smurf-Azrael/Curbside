8685f083ecef9a5812ef9f6db71dc12364175067
Alex Nussbaumer <alex.nssbmr@gmail.com>
2022-05-11 16:47:00 +0200

    feat: get listings route tested

--- server/tests/listsings-tests/listings-test.ts
+++ server/tests/listsings-tests/listings-test.ts
@@ -1,6 +1,8 @@
+import { listingsGetTests } from './listings-get-tests';
 import { listingsPostTests } from './listings-post-tests';
 export const listingsTests = async (): Promise<void> => {
   describe('/listings', () => {
     listingsPostTests();
+    listingsGetTests();
   });
 };
