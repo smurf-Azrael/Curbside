be882e732cf37ccefe20e82d79cf197e290a2b42
smurf-Azrael <phillwang117@gmail.com>
2022-05-12 14:56:50 +0200

    feat: listing detail page format done

---
+++ client/src/components/ImageCarousel.tsx
@@ -0,0 +1,22 @@
+//@ts-nocheck
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import '../styling/ImageCarousel.css';
+
+const ImageCarousel = ({carouselItems}) => {
+  return (
+    <Carousel>
+      {carouselItems.map(function(item) {
+        return <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={item}
+            alt="item for sale"
+          />
+        </Carousel.Item>
+      })}
+    </Carousel>
+  )
+}
+
+export default ImageCarousel
--- client/src/views/ListingDetailView.tsx
+++ client/src/views/ListingDetailView.tsx
@@ -3,36 +3,48 @@ import React from 'react'
 import '../styling/ListingDetail.css';
 import ButtonSmall from '../components/ButtonSmall';
 import SimpleMap from '../components/SimpleMap';
+import ImageCarousel from '../components/ImageCarousel';
 
 // --- setup for mock data
 import { mocks } from '../mocks';
 const USER_ID = '931d461e-bab4-4c7a-be90-e5e1ab611c0f';
 const LISTING_ID = 5;
 const mockListing = mocks.listings.filter(listing => listing.userId === USER_ID && listing.id === LISTING_ID)[0];
+const mockUser = mocks.Users.filter(user => user.id === USER_ID)[0];
+const profileImageTEMP = mockUser.photoUrl;
 // ---
 
+const nameToDisplay = `${mockUser.firstName} ${mockUser.lastName.slice(0,1)}.`;
+const imageUrlArray: string[] = mockListing.photoUrls;
+console.log(imageUrlArray)
 const position = {
   lng: mockListing.longitude,
   lat: mockListing.latitude
 };
 
 
+
 const ListingDetailView = () => {
+  // Need to add chat button link to chat
+  // Need to add heart button functionality
+
   return (
     <section className='listing-detail-container'>
       <section className='listing-owner-info-wrapper'>
         <section className='listing-owner-image-wrapper'>
-          img here
+          <img src={profileImageTEMP} alt={'user'} />
         </section>
         <section className='listing-owner-name-buttons-wrapper'>
-          <p style={{fontWeight:"bold"}}>Name L.</p>
+          <p style={{fontWeight:"bold"}}>{nameToDisplay}</p>
           <i className="bi bi-heart-fill" style={{fontSize:"1.3rem", color:"gray"}}></i>
         </section>
       </section>
       <ButtonSmall content={'Start a chat to buy'} fill={true} />
-      {/* Image section to be added here */}
+      <section className='listing-details-gallery-wrapper'>
+        <ImageCarousel carouselItems={imageUrlArray} />
+      </section>
       <section className='listing-details-data-wrapper'>
-        <p style={{fontWeight:"bold"}}>{mockListing.title}</p>
+        <h4 style={{fontWeight:"bold"}}>{mockListing.title}</h4>
         <p><span style={{fontWeight:"bold", color:"gray"}}>Condition:</span>{mockListing.condition}</p>
         <p><span style={{fontWeight:"bold", color:"gray"}}>Description: </span>{mockListing.description}</p>
         <p style={{fontWeight:"bold", color:"gray"}}>Location: </p>
