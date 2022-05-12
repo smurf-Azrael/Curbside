9f1a8cfb736fa547be573a47d04025388da217cb
ms-aija <aija.avota@gmail.com>
2022-05-12 11:14:52 +0200

    feat: listing detail map coordinates passed as props

--- client/src/components/SimpleMap.tsx
+++ client/src/components/SimpleMap.tsx
@@ -1,29 +1,21 @@
+// @ts-nocheck
 import React from 'react'
 import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import { Icon } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import '../styling/ListingDetail.css'
-import { mocks } from '../mocks';
 
-const USER_ID = '931d461e-bab4-4c7a-be90-e5e1ab611c0f';
-const LISTING_ID = 5;
-const mockListing = mocks.listings.filter(listing => listing.userId === USER_ID && listing.id === LISTING_ID)[0];
-console.log({mockListing});
-console.log(mockListing.longitude)
-console.log(mockListing.latitude)
-
-// const position = {lng: mockListing.longitude, lat: mockListing.latitude};
-const position = {lng: mockListing.longitude, lat: mockListing.latitude};
 
 const customIcon = new Icon({
   iconUrl: "/icons8-select-24.png",
   iconSize: [17, 17]
 })
 
-const SimpleMap = () => {
+const SimpleMap = ({position}) => {
+  console.log('position in SimpleMap component', position)
+
   return (
     <section className='map-component' >
-      <div className='map' id='map' >
       <MapContainer
         center={position}
         zoom={14}
@@ -36,7 +28,6 @@ const SimpleMap = () => {
         />
         <Marker position={position} icon={customIcon}/>
       </MapContainer>
-      </div>
     </section>
   )
 }
--- client/src/views/ListingDetailView.tsx
+++ client/src/views/ListingDetailView.tsx
@@ -3,7 +3,22 @@ import React from 'react'
 import '../styling/ListingDetail.css';
 import ButtonSmall from '../components/ButtonSmall';
 import SimpleMap from '../components/SimpleMap';
-// import {mocks} from '../mocks';
+
+// --- setup for mock data
+import { mocks } from '../mocks';
+const USER_ID = '931d461e-bab4-4c7a-be90-e5e1ab611c0f';
+const LISTING_ID = 5;
+const mockListing = mocks.listings.filter(listing => listing.userId === USER_ID && listing.id === LISTING_ID)[0];
+console.log({mockListing})
+console.log(typeof mockListing.longitude);
+// ---
+
+const position = {
+  lng: mockListing.longitude,
+  lat: mockListing.latitude
+};
+console.log('position in ListingDetailView component', position)
+
 
 const ListingDetailView = () => {
   return (
@@ -14,18 +29,18 @@ const ListingDetailView = () => {
         </section>
         <section className='listing-owner-name-buttons-wrapper'>
           <p style={{fontWeight:"bold"}}>Name L.</p>
-          <i class="bi bi-heart-fill" style={{fontSize:"1.3rem", color:"gray"}}></i>
+          <i className="bi bi-heart-fill" style={{fontSize:"1.3rem", color:"gray"}}></i>
         </section>
       </section>
       <ButtonSmall content={'Start a chat to buy'} fill={true} />
       {/* Image section to be added here */}
       <section className='listing-details-data-wrapper'>
-        <p style={{fontWeight:"bold"}}>Some cool thing</p>
+        <p style={{fontWeight:"bold"}}></p>
         <p>Condition:</p>
         <p>Description: </p>
         <p>Location: </p>
       </section>
-      <SimpleMap />
+      <SimpleMap position={position} />
     </section>
   )
 }
