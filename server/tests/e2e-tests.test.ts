8bfe0be754b83d94660c4cfb7f520582fa24e045
smurf-Azrael <phillwang117@gmail.com>
2022-05-12 18:12:52 +0200

    fix: test master file impor

--- server/tests/e2e-tests.test.ts
+++ server/tests/e2e-tests.test.ts
@@ -1,6 +1,6 @@
 import { server } from '..';
 import { prisma } from '../prisma/client';
-import { listingsTests } from './listsings-tests/listings-test';
+import { listingsTests } from './listings-tests/listings-test';
 import { usersTests } from './users-tests/users-test';
 
 describe('Backend Server End-To-End Tests', () => {
