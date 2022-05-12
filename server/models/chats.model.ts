219d0325bf7069dce5f60d870599c7a058cf6f97
Alex Nussbaumer <alex.nssbmr@gmail.com>
2022-05-12 17:25:15 +0200

    feat: added be chat

--- server/models/chats.model.ts
+++ server/models/chats.model.ts
@@ -1,14 +1,19 @@
-import { IChat } from '../interfaces/chat.interface';
-import { CreateChatInputDTO } from '../interfaces/chat.interface.dto';
-import chatQueries from '../queries/chatQueries';
-import { addChatInputValidation } from './chats.model.validation';
+// import { IChat } from '../interfaces/chat.interface';
+// import { CreateChatInputDTO } from '../interfaces/chat.interface.dto';
+// import chatQueries from '../queries/chatQueries';
+// import { addChatInputValidation } from './chats.model.validation';
 
-const createChat = async (createChatDetails: CreateChatInputDTO): Promise<IChat> => {
-  await addChatInputValidation(createChatDetails);
-  const chat: IChat = await chatQueries.createChat(createChatDetails);
-  return chat;
-};
+// const createChat = async (createChatDetails: CreateChatInputDTO): Promise<IChat> => {
+//   await addChatInputValidation(createChatDetails);
+//   const chat: IChat = await chatQueries.createChat(createChatDetails);
+//   return chat;
+// };
 
-export default {
-  createChat
-};
+// const getChat = async (chatId: string): Promise<IChat> => {
+//   const chat: IChat = await chatQueries.getChat(chatId);
+//   return chat;
+// };
+
+// export default {
+//   createChat, getChat
+// };
