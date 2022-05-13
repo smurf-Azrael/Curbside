ed9f9c06fe5b8a41f81f06399f3f3c00864ce75e
smurf-Azrael <phillwang117@gmail.com>
2022-05-13 11:46:50 +0200

    feat(filters-home-page): finished filters, correct request sent to back end

--- client/src/components/FiltersComponent.tsx
+++ client/src/components/FiltersComponent.tsx
@@ -1,11 +1,7 @@
 import { Form, FormSelect } from 'react-bootstrap'
 import Button from 'react-bootstrap/Button'
 import Modal from 'react-bootstrap/Modal'
-import {
-  Autocomplete,
-  TextField,
-  FormControl,
-} from '@mui/material';
+import { FormControl } from '@mui/material';
 import AutoCompleteSearch from './AutoCompleteSearch';
 import '../styling/FiltersComponents.css';
 
@@ -17,7 +13,7 @@ export default function FiltersComponent({ filtersAreVisible, closeFiltersModal,
         <Modal.Body className="filter-frame" >
           <Form.Group>
             <Form.Label>Sort By</Form.Label>
-            <FormSelect ref={fields.sortByField}>
+            <FormSelect ref={fields.sortByField} >
               <option value='closest'>Closest</option>
               <option value='newest'>Newest</option>
               <option value='price asc'>Price Asc</option>
@@ -34,30 +30,10 @@ export default function FiltersComponent({ filtersAreVisible, closeFiltersModal,
             </FormSelect>
           </Form.Group>
 
-          {/* <Dropdown>
-            <Dropdown.Toggle>Categories</Dropdown.Toggle>
-          </Dropdown> */}
-
           <Form.Group className="auto-complete-search-frame">
             <Form.Label> Category </Form.Label>
             <FormControl>
-              {/* <AutoCompleteSearch /> */}
-
-              {/* <Autocomplete
-                size="small"
-                multiple
-                limitTags={2}
-                id="multiple-limit-tags"
-                options={['hey', 'hello', 'good morning', 'good night']}
-                getOptionLabel={(option) => option}
-                // defaultValue={['hey', 'hello']}
-                renderInput={(params) => (
-                  <TextField {...params}  placeholder="Favorites" />
-                  )}
-                  sx={{ width: '450px' }}
-                  /> */}
-
-
+              <AutoCompleteSearch SetTagStack={fields.SetTagStack} />
             </FormControl>
           </Form.Group>
 
