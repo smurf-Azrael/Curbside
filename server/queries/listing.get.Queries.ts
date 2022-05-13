c4d540d1d197eee8f16805672707763298aec3c6
Gabe <glev2190@gmail.com>
2022-05-13 20:20:45 +0200

    refactor: move query converter helper function to separate file

--- server/queries/listing.get.Queries.ts
+++ server/queries/listing.get.Queries.ts
@@ -1,55 +1,11 @@
 import { Listing } from '@prisma/client';
 import { CustomError } from '../errors/CustomError.class';
-import { LISTING_PARSING_ERROR, USER_NOT_FOUND } from '../errors/SharedErrorMessages';
-import { IListing, IListingCondition, IListingStatus } from '../interfaces/listing.interface';
+import { USER_NOT_FOUND } from '../errors/SharedErrorMessages';
+import { IListing } from '../interfaces/listing.interface';
+import { IdbSelectUserDetails } from '../interfaces/listings.interface.dto';
 import { IUserInfoSelect } from '../interfaces/user.interface';
 import { prisma } from '../prisma/client';
-
-interface IdbSelectUserDetails{
-  firstName: string| null,
-  lastName: string| null,
-  photoUrl: string | null
-}
-
-const convertDBSelectUserDetailsToDetails = (dbSelectUserDetails: IdbSelectUserDetails): IUserInfoSelect => {
-  const reformatedSelectUserDetails = {
-    userFirstName: dbSelectUserDetails.firstName!,
-    userLastName: dbSelectUserDetails.lastName!,
-    userPhotoUrl: dbSelectUserDetails.photoUrl
-  };
-  return reformatedSelectUserDetails;
-};
-
-const convertDataBaseListingToListing = (dbListing: Listing): IListing => {
-  try {
-    const listing: IListing = {
-      id: dbListing.id,
-      userId: dbListing.userId,
-      title: dbListing.title,
-      description: dbListing.description,
-      priceInCents: dbListing.priceInCents,
-      currency: dbListing.currency,
-      condition: dbListing.condition === 'new'
-        ? IListingCondition.new
-        : dbListing.condition === 'gentlyUsed'
-          ? IListingCondition.gentlyUsed
-          : IListingCondition.used,
-      photoUrls: dbListing.photoUrls,
-      longitude: dbListing.longitude,
-      latitude: dbListing.latitude,
-      status: dbListing.status === 'available'
-        ? IListingStatus.available
-        : dbListing.status === 'reserved'
-          ? IListingStatus.reserved
-          : IListingStatus.sold,
-      createdAt: dbListing.createdAt,
-      tags: dbListing.tags
-    };
-    return listing;
-  } catch (error) {
-    throw new CustomError(LISTING_PARSING_ERROR, 400);
-  }
-};
+import { convertDataBaseListingToListing, convertDBSelectUserDetailsToDetails } from './query-helpers/converter.helpers';
 
 export const getListingsByListingId = async (id: string):Promise<IListing | null> => {
   const dbListing : Listing | null = await prisma.listing.findFirst({
--- server/queries/listing.patch.Queries.ts
+++ server/queries/listing.patch.Queries.ts
@@ -1,40 +1,8 @@
 import { Listing } from '@prisma/client';
-import { CustomError } from '../errors/CustomError.class';
-import { LISTING_PARSING_ERROR } from '../errors/SharedErrorMessages';
-import { IListing, IListingCondition, IListingStatus } from '../interfaces/listing.interface';
+import { IListing } from '../interfaces/listing.interface';
 import { FinalizeListingDTO } from '../interfaces/listings.interface.dto';
 import { prisma } from '../prisma/client';
-
-const convertDataBaseListingToListing = (dbListing: Listing): IListing => {
-  try {
-    const listing: IListing = {
-      id: dbListing.id,
-      userId: dbListing.userId,
-      title: dbListing.title,
-      description: dbListing.description,
-      priceInCents: dbListing.priceInCents,
-      currency: dbListing.currency,
-      condition: dbListing.condition === 'new'
-        ? IListingCondition.new
-        : dbListing.condition === 'gentlyUsed'
-          ? IListingCondition.gentlyUsed
-          : IListingCondition.used,
-      photoUrls: dbListing.photoUrls,
-      longitude: dbListing.longitude,
-      latitude: dbListing.latitude,
-      status: dbListing.status === 'available'
-        ? IListingStatus.available
-        : dbListing.status === 'reserved'
-          ? IListingStatus.reserved
-          : IListingStatus.sold,
-      createdAt: dbListing.createdAt,
-      tags: dbListing.tags
-    };
-    return listing;
-  } catch (err) {
-    throw new CustomError(LISTING_PARSING_ERROR, 400);
-  }
-};
+import { convertDataBaseListingToListing } from './query-helpers/converter.helpers';
 
 export const updateListingQuery = async (listingId:string, listingDetails:FinalizeListingDTO): Promise<IListing> => {
   const dbListing: Listing = await prisma.listing.update({
