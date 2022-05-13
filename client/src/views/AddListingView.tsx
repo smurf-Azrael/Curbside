ed5bfe9196e1c235bbefd69b2d1c938cda6cf58f
smurf-Azrael <phillwang117@gmail.com>
2022-05-13 12:01:01 +0200

    feat: added firebase storage

--- client/src/views/AddListingView.tsx
+++ client/src/views/AddListingView.tsx
@@ -1,51 +1,70 @@
-import { useRef, useState, useEffect, FormEvent } from "react"
-import InputField from "../components/InputField"
-import { useAuth } from "../contexts/AuthContext";
-import { useApi } from "../contexts/ApiProvider";
-import { useNavigate } from "react-router-dom";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import { useRef, useState, useEffect, FormEvent } from 'react';
+import InputField from '../components/InputField';
+import { useAuth } from '../contexts/AuthContext';
+import { useApi } from '../contexts/ApiProvider';
+import { useNavigate } from 'react-router-dom';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 import '../styling/AddListingView.css';
-import ButtonWide from "../components/ButtonWide";
-import { FormSelect, Form } from "react-bootstrap";
-
+import ButtonWide from '../components/ButtonWide';
+import { FormSelect, Form } from 'react-bootstrap';
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import { storage } from '../firebase';
 export default function AddListingView() {
   const titleField = useRef<HTMLInputElement>(null);
   const descriptionField = useRef<HTMLInputElement>(null);
   const conditionField = useRef<HTMLSelectElement>(null);
   const priceField = useRef<HTMLInputElement>(null);
-  const [formErrors, setFormErrors] = useState<{ [key: string]: string }>({})
+  const photoField = useRef<HTMLInputElement>(null);
+  const [files, setFiles] = useState([]);
+  const [formErrors, setFormErrors] = useState<{ [key: string]: string }>({});
+
+  const handleFileSelect = (event: any): void => {
+    console.log('called');
+    console.log(event.target.files);
+    setFiles(event.target.files);
+  };
 
   const api = useApi();
   const navigate = useNavigate();
-  const {currentUser} = useAuth();
+  const { currentUser } = useAuth();
 
-  useEffect(()=>{
-    if (!currentUser){
-      navigate('/')
+  useEffect(() => {
+    if (!currentUser) {
+      navigate('/');
     }
-  },[navigate, currentUser])
+  }, [navigate, currentUser]);
 
   const onSubmit = async (event: FormEvent) => {
-
     event.preventDefault();
 
     const title = titleField.current?.value;
     const description = descriptionField.current?.value;
     const condition = conditionField.current?.value;
     const price = priceField.current?.value;
-    const errors: { [key: string]: string } = {}
+    const errors: { [key: string]: string } = {};
     if (!title) {
-      errors.title = 'Title is required'
+      errors.title = 'Title is required';
     }
     if (!description) {
-      errors.description = 'Description is required'
+      errors.description = 'Description is required';
     }
     if (!price) {
-      errors.price = 'Price is required'
+      errors.price = 'Price is required';
+    }
+    if (files.length === 0) {
+      errors.photos = 'Please upload at least one photo';
+    }
+    const urls: string[] = [];
+    for (let file of files) {
+      // @ts-ignore
+      const imageRef = ref(storage, `images/${currentUser?.id}-${file.name}`);
+      await uploadBytes(imageRef, file);
+      const url = await getDownloadURL(imageRef);
+      urls.push(url);
     }
 
-    setFormErrors(errors)
+    setFormErrors(errors);
 
     if (Object.keys(errors).length > 0) {
       return;
@@ -54,35 +73,33 @@ export default function AddListingView() {
     const newListing = {
       userId: currentUser?.id,
       currency: 'eur',
-      photoUrls: [
-        'https://secure.img1-ag.wfcdn.com/im/54089193/resize-h600-w600%5Ecompr-r85/1231/123110031/Daulton+20%27%27+Wide+Velvet+Side+Chair.jpg',
-        "https://images.unsplash.com/photo-1611464908623-07f19927264e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Z3JlZW4lMjBjaGFpcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
-        "https://images.unsplash.com/photo-1571977796766-578d484a6c25?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Z3JlZW4lMjBjaGFpcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
-      ],
-      latitude: 52.5200,
-      longitude: 13.4050,
-      title, description, condition, priceInCents,
-    }
-    console.log(newListing)
+      photoUrls: urls,
+      latitude: 52.52,
+      longitude: 13.405,
+      title,
+      description,
+      condition,
+      priceInCents,
+      tags: 'car bike',
+    };
+    console.log(newListing);
     const res = await api.post('/listings', newListing);
     if (res.ok) {
-      navigate('/')
+      navigate('/');
     } else {
       //should have errors
-      console.log('ERRRORR')
+      console.log('ERRRORR');
     }
-
-  }
-
+  };
 
   return (
-    <div className="body-page" >
+    <div className="body-page">
       <Header />
       <div className="body-content-background">
         <div className="body-frame">
           <div className="body-frame-padding">
             <form onSubmit={onSubmit}>
-              <h3 className='form-title' >List an item</h3>
+              <h3 className="form-title">List an item</h3>
               <InputField
                 name="title"
                 placeholder="title"
@@ -101,13 +118,12 @@ export default function AddListingView() {
                 fieldref={priceField}
               />
               <p style={{ justifyContent: 'flex-start', marginBottom: '10px' }}>Condition</p>
-              <div style={{marginBottom: '15px'}}>
-
+              <div style={{ marginBottom: '15px' }}>
                 <Form.Group>
                   <FormSelect ref={conditionField}>
-                    <option value='new'>New</option>
-                    <option value='gentlyUsed'>Gently Used</option>
-                    <option value='used'>Used</option>
+                    <option value="new">New</option>
+                    <option value="gentlyUsed">Gently Used</option>
+                    <option value="used">Used</option>
                   </FormSelect>
                 </Form.Group>
 
@@ -120,7 +136,18 @@ export default function AddListingView() {
                 label="Description"
                 error={formErrors.description}
                 fieldref={descriptionField}
-                as='textarea'
+                as="textarea"
+              />
+
+              <InputField
+                name="photos"
+                type="file"
+                multiple={true}
+                placeholder="Photos"
+                label="Photos"
+                error={formErrors.photos}
+                fieldref={photoField}
+                onChange={(e) => handleFileSelect(e)}
               />
 
               {/* <div className='location-div-container'>
@@ -141,18 +168,18 @@ export default function AddListingView() {
               </div> */}
 
               <div className="add-item-buttons">
-                <button type='submit' style={{border:'none', padding:'0'}}>
-                  <ButtonWide content={"Create"} fill={true} />
+                <button type="submit" style={{ border: 'none', padding: '0' }}>
+                  <ButtonWide content={'Create'} fill={true} />
                 </button>
-                <ButtonWide clickFunction={() => navigate('/')} content={"Cancel"} fill={false} />
+                <ButtonWide clickFunction={() => navigate('/')} content={'Cancel'} fill={false} />
               </div>
             </form>
           </div>
         </div>
       </div>
-      <div className='body-footer-container'>
+      <div className="body-footer-container">
         <Footer />
       </div>
     </div>
-  )
+  );
 }
