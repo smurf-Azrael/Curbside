8c837db2009d4cac0c10ceb25bd2dc5c6718c590
Mathieu Tinoco <tinoco.mathieu@gmail.com>
2022-05-14 11:33:38 +0200

    feat(homeview-location): switched to interactive map

--- client/src/components/SimpleMap.tsx
+++ client/src/components/SimpleMap.tsx
@@ -20,7 +20,7 @@ const SimpleMap = ({position, radius}: {position: {lat: number, lng: number}, ra
         scrollWheelZoom={true}
         style={{ height: '200px', width: '100%' }}
       >
-        <Circle center={[position.lat, position.lng]} radius={radius * 1000 || 10000} />
+        {radius && <Circle center={[position.lat, position.lng]} radius={radius * 1000 || 10000} />}
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
           url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
