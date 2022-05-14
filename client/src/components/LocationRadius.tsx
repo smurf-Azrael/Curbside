af0ed28551bc387b27ef404be64744d0dcfdb615
Mathieu Tinoco <tinoco.mathieu@gmail.com>
2022-05-14 15:46:04 +0200

    feat(locaion-radius): commiting before pulling

--- client/src/components/LocationRadius.tsx
+++ client/src/components/LocationRadius.tsx
@@ -2,28 +2,33 @@
 import React, { useState, useRef, useEffect } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import { Form, Col, Row, InputGroup, FormControl } from 'react-bootstrap';
-import RangeSlider from 'react-bootstrap-range-slider';
+// import RangeSlider from 'react-bootstrap-range-slider';
 import InputField from './InputField';
 import Button from 'react-bootstrap/Button';
 import Map from './SetProfileMap';
 
-export default function LocationRadius({ locationIsVisible, closeLocationModal }: { [key: string]: any }) {
-  const [radius, setRadius] = useState(25);
+export default function LocationRadius({
+  locationIsVisible,
+  closeLocationModal,
+  setLocationGroupField
+}: { [key: string]: any }) {
+
+  const urlSearch = 'https://nominatim.openstreetmap.org/search?format=json&q='
+  const [radius, setRadius] = useState<number>(25);
   const [position, setPosition] = useState({ lat: 50, lng: 10 });
   const [locationResult, setLocationResult] = useState({});
-  const [clickPosition, setClickPosition] = useState({})
+  const [clickPosition, setClickPosition] = useState<{lat: number | undefined, lng: number | undefined} >({lat: undefined, lng: undefined});
 
-  const locationField = useRef<HTMLInputElement>(null);
+  // const locationField = useRef<HTMLInputElement>();
+const [address, setAddress] = useState<string>();
 
-  function pressEnter(event) {
+  function pressEnter(event: React.KeyboardEvent<HTMLInputElement>) {
     if (event.key === 'Enter') {
-      getCityInformation(locationField.current?.value)
+      getCityInformation(address)
     }
   }
 
-  const urlSearch = 'https://nominatim.openstreetmap.org/search?format=json&q='
-
-  async function getCityInformation(query) {
+  async function getCityInformation(query: string | undefined) {
     const selectedPlace = await fetch(urlSearch + query)
       .then(res => res.json())
       .then(res => formatResponse(res))
@@ -36,7 +41,7 @@ export default function LocationRadius({ locationIsVisible, closeLocationModal }
       ))
   }
 
-  function formatResponse(response) {
+  function formatResponse(response: ApiResponse[]) {
     let selectedPlace = response.filter(el => (el.type === 'city' || el.type === 'village'))[0];
     if (!selectedPlace) {
       selectedPlace = response[0];
@@ -45,20 +50,19 @@ export default function LocationRadius({ locationIsVisible, closeLocationModal }
   }
 
   useEffect(() => {
-    const urlSearch = 'https://nominatim.openstreetmap.org/search?format=json&q='
-    if (clickPosition && clickPosition.lat) {
+    if (clickPosition && clickPosition?.lat) {
       const response = async () => {
-        const latCopy = clickPosition.lat.toString();
-        const lngCopy = clickPosition.lng.toString();
+        const latCopy = clickPosition.lat?.toString();
+        const lngCopy = clickPosition.lng?.toString();
         const positionLessAccurate = [
-          latCopy.slice(0, latCopy.indexOf('.') + 5),
-          lngCopy.slice(0, lngCopy.indexOf('.') + 5)
+          latCopy?.slice(0, latCopy?.indexOf('.') + 5),
+          lngCopy?.slice(0, lngCopy?.indexOf('.') + 5)
         ]
         await fetch(urlSearch + positionLessAccurate.join(','))
           .then(res => res.json())
           .then(res => formatResponse(res))
           .then(res => (
-            locationField.current.value = res.display_name
+            setAddress(res.display_name)
           ))
           .then(() => setPosition(clickPosition));
       };
@@ -67,10 +71,24 @@ export default function LocationRadius({ locationIsVisible, closeLocationModal }
   }, [clickPosition])
 
   useEffect(() => {
-    if (locationResult.lat && locationResult.lng) {
-      setPosition({ lat: locationResult.lat, lng: locationResult.lng })
+
+    if (locationResult.lat && locationResult?.lng) {
+      setPosition({ lat: locationResult?.lat, lng: locationResult?.lng });
     }
-  }, [locationResult])
+  }, [locationResult]);
+
+  useEffect(() => {
+    
+    if (position.lat && position.lng && address) {
+      const locationData = {
+        lat: position.lat,
+        lng: position.lng,
+        radius: radius,
+        address: address
+      }
+      setLocationGroupField(locationData)
+    }
+  }, [position, radius])
 
   return (
     <Modal show={locationIsVisible} onHide={closeLocationModal}>
@@ -81,36 +99,33 @@ export default function LocationRadius({ locationIsVisible, closeLocationModal }
           name="location"
           label='Location'
           type='location'
-          fieldref={locationField}
+          // fieldref={locationField}
+          value={address}
+          onChange={(e) => {console.log('inhere', e.target.value); setAddress(e.target.value)}}
           onKeyPress={e => pressEnter(e)}
         />
-
 
         <Form>
           <Form.Group as={Row}>
             <Col xs="9">
-              <RangeSlider
+              {/* <RangeSlider
                 value={radius}
-                onChange={e => setRadius(e.target.value < 1 ? '1' : e.target.value)}
-              />
+                onChange={e => setRadius(parseInt(e.target.value, 10) < 1 ? 1 : parseInt(e.target.value, 10))}
+              /> */}
             </Col>
             <Col xs="3">
               <InputGroup className="mb-3">
                 <FormControl
                   value={radius}
-                  onChange={e => setRadius(e.target.value < 1 ? '1' : e.target.value)}
+                  onChange={e => setRadius(parseInt(e.target.value, 10) < 1 ? 1 : parseInt(e.target.value, 10))}
                 />
                 <InputGroup.Text style={{ marginRight: '0' }} id="basic-addon2">km</InputGroup.Text>
               </InputGroup>
-              {/* <Form.Control
-                value={radius+'  km'}
-                onChange={e => setRadius(e.target.value.slice(0, e.target.value.length-4))} /> */}
             </Col>
           </Form.Group>
         </Form>
 
         <Map position={position} setPosition={setClickPosition} radius={radius} />
-
 
       </Modal.Body>
       <Modal.Footer>
@@ -121,4 +136,18 @@ export default function LocationRadius({ locationIsVisible, closeLocationModal }
   )
 
 
+}
+interface ApiResponse {
+  "place_id": number,
+  "licence": string,
+  "osm_type": string,
+  "osm_id": number,
+  "boundingbox": string[],
+  "lat": string,
+  "lon": string,
+  "display_name": string,
+  "class": string,
+  "type": string,
+  "importance": number,
+  "icon": string
 }
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
-import 'react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
