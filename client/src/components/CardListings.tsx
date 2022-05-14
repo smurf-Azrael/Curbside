32cc6521f944d2a6a9e8a8e41818aa3dd5a78b42
smurf-Azrael <phillwang117@gmail.com>
2022-05-14 17:21:12 +0200

    refactor: moved card listing to its own component

---
+++ client/src/components/CardListings.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Listing } from '../interfaces/Listing'
+import ListingPreview from './ListingPreview'
+import loader from '../assets/loader.gif';
+
+
+function CardListings({listings}:{listings:Listing[]}, isLoading:boolean,loadingError:boolean) {
+  console.log("I'm in CardListings",listings)
+  const listings4Display= listings.map(listing => {
+    return (<Link key={listing.id} to={`/listing/${listing.id}`} style={{ textDecoration: "none", color: "black" }}>
+      <ListingPreview  listing={listing} />
+    </Link>)
+  })
+  return (
+    <div className='listings-container'>
+      <div>{listings4Display}</div>
+      <div>
+        {!isLoading && listings.length === 0 && <p>No listing matched your request...</p>}
+        {loadingError && <p>Couldn't load listings :/</p>}
+        {isLoading && <img style={{ height: '20vw', maxHeight: '200px', borderRadius: '20px' }} src={loader} alt="Loading..." />} 
+      </div>
+    </div>
+  )
+}
+
+export default CardListings
