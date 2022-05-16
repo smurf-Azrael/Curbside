c5001d3771c94a149d98e1ffae505aea33272372
smurf-Azrael <phillwang117@gmail.com>
2022-05-16 21:05:29 +0200

    feat(header-logout): working on a log out message

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -4,6 +4,8 @@ import ButtonSmall from './ButtonSmall';
 import curbside from './../assets/CurbsideSmall.png';
 import { useAuth } from '../contexts/AuthContext';
 import { useNavigate } from 'react-router-dom';
+import Modal from 'react-bootstrap/Modal';
+
 
 export default function Header({ prevRoute, }: HeaderProps) {
   const navigate = useNavigate();
@@ -11,29 +13,43 @@ export default function Header({ prevRoute, }: HeaderProps) {
 
 
   function PrevRoute() {
-    return ( 
-      <button 
+    return (
+      <button
         className='PrevRoute'
-        onClick={()=> navigate(-1)}
+        onClick={() => navigate(-1)}
       >
         <i className="bi bi-arrow-left"></i>
-      </button> 
+      </button>
     )
   }
+
+  function loggingOut() {
+    console.log('youre a genius')
+    
+    //logOut()
+  }
+
   return (
     <div className='Header'>
-      {prevRoute && <PrevRoute /> }
+      {prevRoute && <PrevRoute />}
       <button className="curbside-home" onClick={() => navigate('/')}>
         <img src={curbside} alt='Curbside' />
       </button>
       {currentUser ?
-        (<div className='header-login' onClick={() => logOut()} >
+        (<div className='header-login' onClick={loggingOut} >
           <ButtonSmall content={'Log Out'} fill={false} />
         </div>) :
         (<div className='header-login' onClick={() => navigate('/login')} >
           <ButtonSmall content={'Log in'} fill={true} />
         </div>)
       }
+
+      <Modal show={true}>
+        <Modal.Body>
+          <p>Hi!!</p>
+        </Modal.Body>
+      </Modal>
+
     </div>
   )
 }
