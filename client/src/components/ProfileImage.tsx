b2f277077327c06da06a4ab76824f2426d6a2e6b
Mathieu Tinoco <tinoco.mathieu@gmail.com>
2022-05-17 21:25:23 +0200

    feat(set-profile): implemented address search

--- client/src/components/ProfileImage.tsx
+++ client/src/components/ProfileImage.tsx
@@ -3,7 +3,6 @@ import { useNavigate } from "react-router-dom"
 
 const ProfileImage = ({ user }: { user: { id:string, photoUrl?: string, firstName: string } }) => {
   const navigate = useNavigate()
-  console.log(user)
   return (
     <div onClick={()=> navigate(`/profile/${user.id}`)} className="ProfileImage" style={{ backgroundImage: user.photoUrl ? `url("${user.photoUrl}")` : `url("https://gradient-avatar.glitch.me/${user.id}")` }}>
       <span>
