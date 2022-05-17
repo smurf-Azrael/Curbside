67daf2b40f51cbf8ae4b81bdf4b4030a0081d9c8
RR <roznoshchik@gmail.com>
2022-05-17 11:36:24 +0200

    fix: loading on listing and profile, added login back

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,10 +1,14 @@
 import '../styling/Header.scss';
 import curbside from './../assets/CurbsideSmall.png';
 import { useNavigate } from 'react-router-dom';
-import tree from "../assets/tree.gif"
+import tree from "../assets/tree.gif";
+import { useAuth } from '../contexts/AuthContext';
+import ButtonSmall from './ButtonSmall';
 
 export default function Header({ prevRoute, }: HeaderProps) {
   const navigate = useNavigate();
+  const { currentUser } = useAuth()
+
   const baseDate = new Date(2022, 4, 17, 10, 21, 32); // May 17, 2022
   const now = new Date();
   let diffInHours = Math.round(Math.abs(baseDate.getTime() - now.getTime()) / (60*60*1000));
@@ -26,7 +30,13 @@ export default function Header({ prevRoute, }: HeaderProps) {
       <button className="curbside-home" onClick={() => navigate('/')}>
         <img src={curbside} alt='Curbside' />
       </button>
-      <p className='treesPlanted'><img src={tree} alt="trees" />{treesPlanted}!</p>
+      {currentUser ?
+        (<p className='treesPlanted'><img src={tree} alt="trees" />{treesPlanted}!</p>) :
+        (<div className='header-login' onClick={() => navigate('/login')} >
+          <ButtonSmall content={'Log in'} fill={true} />
+        </div>)
+      }
+      
     </div>
   )
 }
