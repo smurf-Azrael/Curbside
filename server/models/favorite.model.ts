82a43ad48f190611c9f2f8e2f80140c4051c0d9c
smurf-Azrael <phillwang117@gmail.com>
2022-05-17 21:10:31 +0200

    feat: add data transfert of frontend for addFavorites route

--- server/models/favorite.model.ts
+++ server/models/favorite.model.ts
@@ -4,6 +4,7 @@ import favoriteQueries from '../queries/favorite.queries';
 const addFavorite = async (userId: string, favorites: string): Promise<FavoriteDTO | boolean> => {
   try {
     const favorite: any = await favoriteQueries.addFavorite(userId, favorites);
+    console.log('=========favorite', favorite);
     return favorite;
   } catch (error) {
     console.log('/models/user.model addFavorite ERROR', error);
