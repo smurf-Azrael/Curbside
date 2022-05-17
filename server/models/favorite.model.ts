07d48ee7c45c599c33f2ecb38138cda5b32431ce
smurf-Azrael <phillwang117@gmail.com>
2022-05-17 21:24:31 +0200

    feat: getFavorites /Get route setup

--- server/models/favorite.model.ts
+++ server/models/favorite.model.ts
@@ -6,11 +6,22 @@ const addFavorite = async (userId: string, favorites: string): Promise<FavoriteD
     const favorite: any = await favoriteQueries.addFavorite(userId, favorites);
     return favorite;
   } catch (error) {
-    console.log('/models/user.model addFavorite ERROR', error);
+    console.log('/models/favorites.model addFavorite ERROR', error);
+    throw error;
+  }
+};
+
+const getFavorites = async (userId: string): Promise<FavoriteDTO> => {
+  try {
+    const userFavoritesPackage: any = await favoriteQueries.getFavorites(userId);
+    return userFavoritesPackage;
+  } catch (error) {
+    console.log('/model/favorites.model getFavorites ERROR', error);
     throw error;
   }
 };
 
 export default {
-  addFavorite
+  addFavorite,
+  getFavorites
 };
