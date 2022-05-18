1527c75e930e60cd64de930bd7c831c7112986bb
Gabe-Levin <glev2190@gmail.com>
2022-05-18 19:20:46 +0200

    Revert "Last day fixes"

--- client/src/components/SimpleMap.tsx
+++ client/src/components/SimpleMap.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
-import { MapContainer, TileLayer, Circle } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Circle } from 'react-leaflet';
+import { Icon } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import '../styling/SimpleMap.scss'
 
+const customIcon = new Icon({
+  iconUrl: "/icons8-select-24.png",
+  iconSize: [17, 17]
+})
 
 const SimpleMap = ({position, radius}: {position: {lat: number, lng: number}, radius: number}) => {
 
@@ -10,7 +15,7 @@ const SimpleMap = ({position, radius}: {position: {lat: number, lng: number}, ra
     <section className='SimpleMap' >
       <MapContainer
         center={position}
-        zoom={14}
+        zoom={10}
         scrollWheelZoom={true}
         style={{ height: '200px', width: '100%' }}
       >
@@ -19,6 +24,7 @@ const SimpleMap = ({position, radius}: {position: {lat: number, lng: number}, ra
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
           url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
         />
+        <Marker position={position} icon={customIcon}/>
       </MapContainer>
     </section>
   )
