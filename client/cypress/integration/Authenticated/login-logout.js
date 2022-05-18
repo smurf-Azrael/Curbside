aa19d425188658be4b7f8603ebf6fe6da245ece1
smurf-Azrael <phillwang117@gmail.com>
2022-05-18 18:05:30 +0200

    feat: login logout tests with cypress

--- client/cypress/integration/Authenticated/login-logout.js
+++ client/cypress/integration/Authenticated/login-logout.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import '@testing-library/cypress/add-commands';
 
 Cypress.on('uncaught:exception', (err, runnable) => {
@@ -8,28 +9,28 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 
 describe ('End to end testing with Cypress', () => {
 
-  // it('logs in', () => {
-  //   cy.visit('http://localhost:3000');
-  //   cy.findByRole('button', {  name: /log in/i}).click();
-  //   cy.findByRole('textbox', {  name: /email/i}).clear().type('cypress@curbside.com');
-  //   cy.findByLabelText(/password/i).clear().type('cypress');
-  //   cy.findByRole('button', {  name: /log in/i}).click();
-  // })
-
-  it('add a new listing', () => {
+  it('logs in', () => {
     cy.visit('http://localhost:3000');
-    cy.get('#root > div > div > div:nth-child(3) > a:nth-child(3) > i').click();
-    cy.findByLabelText('Title', {selector: 'input'}).click({force: true}).type('Test title');
-    cy.findByLabelText('Price', {selector: 'input'}).click({force: true}).type(11.22);
-    cy.get('.form-select').select('Used');
-    cy.findByLabelText('Description', {selector: 'textarea'}).type('Test item description');
-    cy.findByLabelText('Photos').selectFile('cypress/fixtures/testPicture.png');
-    cy.findByLabelText('Categories', {selector: 'input'}).type('moto').findByRole('option', {  name: /cars/i}).click();
+    cy.findByRole('button', {  name: /log in/i}).click();
+    cy.findByRole('textbox', {  name: /email/i}).clear().type('cypress@curbside.com');
+    cy.findByLabelText(/password/i).clear().type('cypress');
+    cy.findByRole('button', {  name: /log in/i}).click();
   })
 
-  // it('logs out', () => {
+  // it('adds a new listing', () => {
   //   // cy.visit('http://localhost:3000');
-  //   cy.get('#root > div > div > div:nth-child(3) > button > i').click();
-  //   cy.findByRole('button', {  name: /log out/i}).click();
+  //   cy.get('#root > div > div > div:nth-child(3) > a:nth-child(3) > i').click();
+  //   cy.findByLabelText('Title', {selector: 'input'}).click({force: true}).type('Test title');
+  //   cy.findByLabelText('Price', {selector: 'input'}).click({force: true}).type(11.22);
+  //   cy.get('.form-select').select('Used');
+  //   cy.findByLabelText('Description', {selector: 'textarea'}).type('Test item description');
+  //   cy.findByLabelText('Photos').selectFile('cypress/fixtures/testPicture.png');
+  //   cy.findByLabelText('Categories', {selector: 'input'}).type('ca').findByText('Cars');
   // })
+
+  it('logs out', () => {
+    // cy.visit('http://localhost:3000');
+    cy.get('#root > div > div > div:nth-child(3) > button > i').click();
+    cy.findByRole('button', {  name: /log out/i}).click();
+  })
 })
