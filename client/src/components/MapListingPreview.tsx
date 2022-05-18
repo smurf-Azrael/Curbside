e45aba454d8fea83f44cce7a2df80e49e3491198
Alex Nussbaumer <alex.nssbmr@gmail.com>
2022-05-18 12:27:51 +0200

    refactor: homepage cosmetics 💅

--- client/src/components/MapListingPreview.tsx
+++ client/src/components/MapListingPreview.tsx
@@ -1,34 +1,46 @@
-import React from 'react'
+import React from 'react';
 import { Listing } from '../interfaces/Listing';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import '../styling/MapListingPreview.scss';
+import xBtn from '../assets/x-symbol.svg';
+import { useAuth } from '../contexts/AuthContext';
 
-
-
-function MapListingPreview({ activeListing }: { activeListing: Listing }) {
-
+function MapListingPreview({
+  activeListing,
+  setActiveListing,
+}: {
+  activeListing: Listing;
+  setActiveListing: React.Dispatch<React.SetStateAction<Listing | null | undefined>>;
+}) {
   const price = (activeListing.priceInCents / 100).toLocaleString('en-US', { style: 'currency', currency: 'EUR' });
-
-
+  const { currentUser } = useAuth();
   return (
-    <Link className='Card-Listing-Preview' to={`/listing/${activeListing.id}`}>
-      <img className='Preview-Img' alt={'item for sale'} src= {activeListing.photoUrls[0]} />
-      <div className= 'Preview-Text-Content'>
-        <h6 >
-          {activeListing.title}
-        </h6>
-        <div className='Preview-Price-Listing-Container'>
-          <div className='Preview-Price'>
-            {price}
-          </div>
-          <div className='Preview-Rating'>
-            {activeListing.rating ? activeListing.rating : "New"}
-            <i className="bi bi-star-fill"></i>
+    <div className={`Card-Listing-Preview ${currentUser && 'with-user'}`}>
+      <div
+        className="closeBtn"
+        onClickCapture={() => {
+          setActiveListing(null);
+        }}
+      >
+        <img src={xBtn} alt="close" className="xBtnIcon"></img>
+      </div>
+      <Link className="link-element" to={`/listing/${activeListing.id}`}>
+        <div className="Preview-Image-Container">
+          <img className="Preview-Img" alt={'item for sale'} src={activeListing.photoUrls[0]} />
+        </div>
+        <div className="Preview-Text-Content">
+          <h6>{activeListing.title}</h6>
+          <div className="Preview-Price-Listing-Container">
+            <div className="Preview-Price">{price}</div>
+            <div className="Preview-Rating">
+              {activeListing.rating ? activeListing.rating : 'New'}
+              <i className="bi bi-star-fill"></i>
+            </div>
           </div>
         </div>
-      </div>
-    </Link>
-  )
+      </Link>
+    </div>
+  );
 }
 
-export default MapListingPreview
+export default MapListingPreview;
