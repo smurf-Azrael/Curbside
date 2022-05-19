756672f1d70e52da5c814e98fdb30efc9abcec21
RR <roznoshchik@gmail.com>
2022-05-19 12:13:18 +0200

    refactor: testing and refactoring to simplify views

--- client/src/tests/AddListingForm.test.js
+++ client/src/tests/AddListingForm.test.js
@@ -1,10 +1,10 @@
 import React from "react";
 import '@testing-library/jest-dom'
 import { jest, describe, expect, test, beforeAll } from '@jest/globals'
-import { render, waitFor, screen, fireEvent } from '@testing-library/react'
+import { render, waitFor, screen, fireEvent, act, waitForElementToBeRemoved, findByAltText } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 
-import AddListingView from "../views/AddListingView";
+import AddListingForm from "../components/AddListingForm"
 
 
 jest.mock('../firebase', () => {
@@ -19,28 +19,32 @@ jest.mock('../contexts/AuthContext', () => {
     useAuth: () => {
       const currentUser = { 
         id: "1", 
-        latitude: 30.30,
-        longitude: 13.13
       } 
-      return   currentUser 
+      return { currentUser } 
     }
   }
 })
 
+const mockPost = jest.fn(async (url, body) => {
+  console.log('hhhhhhhhhh')
+  console.log(body)
+  return {ok : true}
+})
 
 jest.mock('../contexts/ApiProvider', () => {
   return {
     useApi: () => {
       const api = { 
-        get: (url, query, options) => {
+        get: async (url, query, options) => {
           console.log(url)
-          console.log(query)
+          return {ok: true, body: { data: { user: {latitude: 30.30, longitude: 13.13}}}}
         }, 
-        post: (url, body) => {
-          console.log(url)
-          console.log(body)
-          return {ok : true}
-        }
+        post: () => mockPost()
+        // post: async (url, body) => {
+        //   console.log(url)
+        //   console.log(body)
+        //   return {ok : true}
+        // }
       } 
       return api
     }
@@ -56,10 +60,10 @@ jest.mock('react-router-dom', () => {
   }
 })
 
-describe('Add Listing View', ()=>{
+describe('Add Listing Form', ()=>{
   it('loads and displays Add Listing View', async () => {
-    render(<AddListingView />);
-    
+    render(<AddListingForm />);
+
     await waitFor(()=> {
       screen.getByLabelText('Title');
       screen.getByLabelText('Price');
@@ -72,7 +76,10 @@ describe('Add Listing View', ()=>{
   })
 
   it('displays errors when missing info', async () => {
-    render(<AddListingView />);
+    render(<AddListingForm />);
+    await waitForElementToBeRemoved(()=> screen.queryByAltText('spinner'))
+
+
     const title = screen.getByLabelText(/title/i);
     const price = screen.getByLabelText(/price/i);
     const condition = screen.getByLabelText(/condition/i);
@@ -81,33 +88,48 @@ describe('Add Listing View', ()=>{
     const photos = screen.getByLabelText(/photos/i)
     const submit = screen.getByRole('button', {name : /create/i})
     
+
     userEvent.click(submit);
+
+    
     expect(screen.getByText(/Title is required/i)).toBeTruthy()
     expect(screen.getByText(/Price is required/i)).toBeTruthy()
     expect(screen.getByText(/Description is required/i)).toBeTruthy()
     expect(screen.getByText(/Please upload at least one photo/i)).toBeTruthy()
     expect(screen.getByText(/Please add at least one category/i)).toBeTruthy()
     
+
+
     userEvent.type(title, 'Awesome Sauce')
+
+  
     userEvent.click(submit);
+
     expect(()=>screen.getByText(/Title is required/i)).toThrowError('Unable to find an element with the text: /Title is required/i')
     
     userEvent.type(categories, 'moto')
     const option = screen.getByText('Motorbikes')
     userEvent.click(option);
     userEvent.click(submit);
+
     expect(()=>screen.getByText(/Please add at least one category/i)).toThrowError('Unable to find an element with the text: /Please add at least one category/i')
     
     const file = new File(['(⌐□_□)'], 'chucknorris.png', { type: 'image/png' })
     fireEvent.change(photos, { target :{ files:file }})
     userEvent.click(submit);
+    
     expect(()=>screen.getByText(/Please upload at least one photo/i)).toThrowError('Unable to find an element with the text: /Please upload at least one photo/i')
-  
-  
+    
+
   })
 
-   it('passes the correct query to the api', ()=> {
-    render(<AddListingView />);
+   it('passes the correct query to the api', async ()=> {
+     
+    
+    const promise = Promise.resolve();
+    render(<AddListingForm />);
+    await act(async ()=> await promise);
+
     const title = screen.getByLabelText(/title/i);
     const price = screen.getByLabelText(/price/i);
     const condition = screen.getByLabelText(/condition/i);
@@ -126,15 +148,16 @@ describe('Add Listing View', ()=>{
     userEvent.type(categories, 'moto')
     const option = screen.getByText('Motorbikes')
     userEvent.click(option);
-    
 
+    
     userEvent.click(submit);
     expect(() => screen.getByText(/Title is required/i)).toThrowError('Unable to find an element with the text: /Title is required/i')
     expect(() => screen.getByText(/Price is required/i)).toThrowError('Unable to find an element with the text: /Price is required/i')
     expect(() => screen.getByText(/Description is required/i)).toThrowError('Unable to find an element with the text: /Description is required/i')
     expect(() => screen.getByText(/Please upload at least one photo/i)).toThrowError('Unable to find an element with the text: /Please upload at least one photo/i')
     expect(() => screen.getByText(/Please add at least one category/i)).toThrowError('Unable to find an element with the text: /Please add at least one category/i')
-
+    
+    await waitForElementToBeRemoved(()=> screen.queryByAltText('spinner'))
    })
 
 
