26a5f3eb2dccefc482239c0e2169e8990dd029a9
Mathieu Tinoco <tinoco.mathieu@gmail.com>
2022-05-22 18:39:13 +0200

    test(testing-ListingPreview.test.js): testing

--- client/src/tests/ButtonSmall.test.js
+++ client/src/tests/ButtonSmall.test.js
@@ -1,7 +1,7 @@
 import { screen, render } from "@testing-library/react";
 import ButtonSmall from "../components/ButtonSmall";
 
-test('should render input field', ()=> {
+test('should render button', ()=> {
   render(<ButtonSmall content={'button1'} fill={true}/>);
   screen.getByText('button1');
   expect(screen.getByText('button1')).toHaveClass('fill');
